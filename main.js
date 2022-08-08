7b120a3ba85bcd1ccfaa96e921b1d4c50e47717f
Gabriel de Azevedo Cunha <gabrielazevedoc15@hotmail.com>
2022-08-08 09:21:47 -0300

    parcial finish

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
-function apagarvalue(){
-    document.getElementsByTagName("input")[0,1,2].value = ""
+function apagarvalue1(){
+    document.getElementsByTagName("input")[0].value = "";
+}
+
+function apagarvalue2(){
+    document.getElementsByTagName("input")[1].value = "";
+}
+
+function apagarvalue3(){
+    document.getElementsByTagName("input")[2].value = "";
 }
