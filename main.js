fda267c51d7c053e1dca9fdf559c6305310d1b55
Gabriel de Azevedo Cunha <gabrielazevedoc15@hotmail.com>
2022-08-22 10:22:03 -0300

    mudança form

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
 
-// function apagarvalue1(){
-//     document.getElementsByTagName("input")[0].value = "";
-// }
-
-// function apagarvalue2(){
-//     document.getElementsByTagName("input")[1].value = "";
-// }
-
-// function apagarvalue3(){
-//     document.getElementsByTagName("input")[2].value = "";
-// }
