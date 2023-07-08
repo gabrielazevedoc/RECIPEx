2e4161c886f7eeb3bbce10e478c8ad9b04d2308a
Gabriel de Azevedo Cunha <101353668+gabrielazevedoc@users.noreply.github.com>
2023-07-08 15:11:24 -0300

    menu home javascript

--- Home/main.js
+++ Home/main.js
@@ -1,2 +1,13 @@
+// Aparecer menu quando tocar no Icone do Hamburguer
 
+const menu = document.querySelector('#menu')
+const menuWrapper = document.querySelector('.menu-wrapper')
+
+menu.onmouseover = function(){
+    menuWrapper.classList.add('open')
+}
+
+menu.onmouseout = function(){
+    menuWrapper.classList.remove('open')
+}
 
