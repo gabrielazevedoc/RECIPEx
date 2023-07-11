712b1e2544428bf0378f6dd028fe9a895829d6ad
Gabriel de Azevedo Cunha <101353668+gabrielazevedoc@users.noreply.github.com>
2023-07-11 18:04:18 -0300

    ajeitando home page

--- Home/main.js
+++ Home/main.js
@@ -1,13 +1,16 @@
 // Aparecer menu quando tocar no Icone do Hamburguer
 
 const menu = document.querySelector('#menu')
+const buttonMenu = document.querySelector('#menu i')
 const menuWrapper = document.querySelector('.menu-wrapper')
 
 menu.onclick = function(){
     menuWrapper.classList.toggle('open')
+
+    const isOpen = menuWrapper.classList.contains('open')
+    buttonMenu.classList = isOpen
+    ? 'fa-regular fa-x'
+    : 'fa-solid fa-bars'
 }
 
-// menu.onmouseout = function(){
-//     menuWrapper.classList.remove('open')
-// }
 
