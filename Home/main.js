1384deff17ff6a6620d1f439db7c02da741a3b5c
Gabriel de Azevedo Cunha <101353668+gabrielazevedoc@users.noreply.github.com>
2023-07-16 06:39:46 -0300

    Update main.js

--- Home/main.js
+++ Home/main.js
@@ -3,14 +3,18 @@
 const menu = document.querySelector('#menu')
 const buttonMenu = document.querySelector('#menu i')
 const menuWrapper = document.querySelector('.menu-wrapper')
+const sections = document.querySelector('.sections')
 
-menu.onclick = function(){
-    menuWrapper.classList.toggle('open')
+menu.onclick = handleMenuButton
+
+function handleMenuButton(){
+     menuWrapper.classList.toggle('open')
 
     const isOpen = menuWrapper.classList.contains('open')
     buttonMenu.classList = isOpen
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
 }
+
 
 
