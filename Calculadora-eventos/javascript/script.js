25ffe4bd9187f9f96bb1fe00c6316a461fb5df59
Gabriel de Azevedo Cunha <101353668+gabrielazevedoc@users.noreply.github.com>
2023-09-30 17:35:55 -0300

    Update script.js

--- Calculadora-eventos/javascript/script.js
+++ Calculadora-eventos/javascript/script.js
@@ -10,8 +10,8 @@ const inputTotalCriancas = document.querySelector('#criancas')
 
 form.onsubmit = showModalResult
 
-function showModalResult(event){
-    event.preventDefault()
+function showModalResult(){
+    Event.preventDefault()
 
     const tipoEvento = inputTipoEvento.value
     const totalAdultos = inputTotalAdultos.value
