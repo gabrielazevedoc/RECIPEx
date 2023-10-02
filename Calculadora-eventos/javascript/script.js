69caee2399323c284a4d3f1ae54895b036ef7d4b
Gabriel de Azevedo Cunha <101353668+gabrielazevedoc@users.noreply.github.com>
2023-10-02 19:00:10 -0300

    changes

--- Calculadora-eventos/javascript/script.js
+++ Calculadora-eventos/javascript/script.js
@@ -1,16 +1,42 @@
-import { modal } from "./modal.js"
-import { calculate } from "./utils.js"
+const form = document.querySelector("#form")
+form.addEventListener("submit", showEventResult())
 
-document.querySelector("form").onsubmit = showResult
 
-function showResult(event){
+const modal = {
+    wrapper: document.querySelector('.modal'),
+    h1: document.querySelector('.modal h1'),
+    text: document.querySelector('.modal p'),
+    modalButton: document.querySelector('.modal i'),
+
+    open() {
+        modal.wrapper.classList.add('open')
+    },
+
+    close(){
+        modal.wrapper.classList.remove('open')
+    }
+}
+
+modal.modalButton.onclick = function(){
+    modal.close()
+}
+
+function showEventResult(event){
     event.preventDefault()
 
-    const eventType = document.querySelector('#eventType').value
-    const totalAdults = Number(document.querySelector('#adults').value)
-    const totalChildren = Number(document.querySelector('#children').value)
+    const eventType = document.querySelector("#eventType").value
+    const totalAdults = document.querySelector("#adults").value
+    const totalChildren = document.querySelector("#children").value
 
-    modal.text.innerText = calculate()
+    modal.text.innerText = calculate(totalAdults, totalChildren)
     modal.h1.innerText = eventType
     modal.open()
+
+}
+
+function calculate(totalAdults, totalChildren){
+    return `Total de comida necessária: ${(totalAdults + totalChildren)*0.5}
+            Total de bebida necessária: ${(totalAdults + totalChildren*0.3)}`
+
+    console.log("123")
 }
--- Calculadora-eventos/javascript/utils.js
+++
@@ -1,9 +0,0 @@
-export function calculate(totalAdults,totalChildren){
-    let eventFood = parseInt(totalAdults + totalChildren) * 0.7
-    let eventDrink = parseInt(totalAdults + totalChildren) * 0.5
-
-    const result = "A quantidade total de comida para o seu evento é de " + eventFood + " kg e\n a quantidade total de bebida é " + eventDrink +" L"
-
-    return result
-}
-
