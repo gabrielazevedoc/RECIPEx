9022733cf4aafc5108f38dc0f0eda26067c6e3c1
Gabriel de Azevedo Cunha <101353668+gabrielazevedoc@users.noreply.github.com>
2024-04-15 12:15:19 -0300

    feat: "footer component"

--- recipex-app/src/App.jsx
+++ recipex-app/src/App.jsx
@@ -1,7 +1,8 @@
-import './App.css';
+
 
 import NavBar from './components/NavBar';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <NavBar/>
       <Header/>
+      <Footer/>
     </>
   );
 }
---
+++ recipex-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+const Footer = () => {
+    return(
+        <div className="footer-conteiner">
+            <p>Site desenvolvido por Gabriel Azevedo</p>
+        </div>
+    )
+}
+
+export default Footer;
