2fba0524aafa878bc9a08b593f09cbc36f87ba36
Gabriel de Azevedo Cunha <101353668+gabrielazevedoc@users.noreply.github.com>
2024-04-19 10:34:50 -0300

    feat: starting add Recipe Category card component

---
+++ recipex-app/src/components/RecipeCategoryCard.jsx
@@ -0,0 +1,7 @@
+const RecipeCategoryCard = (props) => {
+    return (
+        <div className="category-container">
+            <h3 className="category-title">{props.title}</h3>
+        </div>
+    )
+}
