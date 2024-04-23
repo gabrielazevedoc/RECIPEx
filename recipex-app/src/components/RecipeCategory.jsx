38cc77a3c326e41646cea91780289862df6560fb
Gabriel Azevedo <gabrielazevedoc15@hotmail.com>
2024-04-22 21:06:02 -0300

    feat: add Recipe Category components

--- recipex-app/src/components/RecipeCategory.jsx
+++ recipex-app/src/components/RecipeCategory.jsx
@@ -1,6 +1,10 @@
-import MeatImg from '../assets/images/meat-img-card.jpg'
+import MeatImg from '../assets/images/meat-img-card.jpg';
+import ChickenImg from '../assets/images/chicken-img-card.jpg';
+import FishImg from '../assets/images/fish-img-card.jpg';
+import VegetablesImg from '../assets/images/vegetables-img-card.jpg';
+import DessertImg from '../assets/images/dessert-img-card.jpg';
 
-import RecipeCategoryCard from '../components/RecipeCategoryCard';
+import RecipeCategoryCard from './RecipeCategoryCard'
 
 import './RecipeCategory.scss';
 
@@ -9,7 +13,11 @@ const RecipeCategory = () => {
         <div className="recipe-category-container">
             <h1>Categorias</h1>
             <div className="recipe-category-cards">
-                <RecipeCategoryCard title={MeatImg} alt="imagem da categoria de carnes"/>
+                <RecipeCategoryCard img={MeatImg} alt="imagem da categoria de carnes" title="Carnes"/>
+                <RecipeCategoryCard img={ChickenImg} alt="imagem da categoria de aves" title="Aves"/>
+                <RecipeCategoryCard img={FishImg} alt="imagem da categoria de peixes" title="Peixes"/>
+                <RecipeCategoryCard img={VegetablesImg} alt="imagem da categoria de vegetais" title="Vegetais"/>
+                <RecipeCategoryCard img={DessertImg} alt="imagem da categoria de sobremesas" title="Sobremesas"/>
             </div>
         </div>
     )
